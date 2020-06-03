a35dba53c072393555d6ef5c1ee14e36a91ded5c
Hadi Firmansyah <61578530+Hadi-Firmansyah@users.noreply.github.com>
2020-06-03 12:50:48 +0700

    Upload Index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"
+    rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!-- Datatable -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+</head>
+    <title>CRUD NO LOADING</title>
+<body>
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquer/3.3.1/jquery.min.js"></script>
+    <!-- Datatable -->
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery/dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript"></script>
+    
+</body>
+</html>
