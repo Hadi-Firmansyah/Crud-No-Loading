f28da36564181aa940317b6007f32d9ad56e8937
Hadi Firmansyah <61578530+Hadi-Firmansyah@users.noreply.github.com>
2020-06-04 09:58:30 +0700

    FIx Problem + Update CDN
    Sebelumnya masih banyak error

--- index.php
+++ index.php
@@ -6,21 +6,20 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"
     rel="stylesheet">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
-    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <!-- Datatable -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+    <!-- Jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.20/js/jquery.dataTables.min.js"></script>
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.js">
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery/dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </head>
     <title>CRUD NO LOADING</title>
 <body>
-    <!-- Jquery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- Datatable -->
-    <link rel="stylesheet" src="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"></link>
-    <!-- <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script> -->
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             $('.data').load("data.php");
@@ -38,21 +37,25 @@
                 }else{
                     document.getElementById("err_nama_siswa").innerHTML = "";
                 }
+
                 if(alamat== ""){
                     document.getElementById("err_alamat").innerHTML = "Alamat Harus Diisi";
                 }else{
                     document.getElementById("err_alamat").innerHTML = "";
                 }
+
                 if(jurusan==""){
                     document.getElementById("err_jurusan").innerHTML = "Jurusan Harus Diisi";
                 }else{
                     document.getElementById("err_jurusan").innerHTML = "";
                 }
+
                 if(tanggal_masuk==""){
                     document.getElementById("err_tanggal_masuk").innerHTML = "Tanggal Masuk Harus Diisi";
                 }else{
                     document.getElementById("err_tanggal_masuk").innerHTML = "";
                 }
+
                 if(document.getElementById("jenkel1").checked==false && document.getElementById("jenkel2").checked==false){
                     document.getElementById("err_jenkel").innerHTML = "Jenis Harus Dipilih";
                 }else{
@@ -60,24 +63,24 @@
                 }
 
                 if(nama_siswa!="" && tanggal_masuk!="" && alamat!="" && jurusan!="" &&
-                (document.getElementById("jenkel1").checked==true || document.getElementById("jenkel2").checked==true))
-    {
-        $.ajax({
-            type : 'POST',
-            url : "form_action.php",
-            data : data,
-            success : function(){
-                $('.data').load("data.php");
-                document.getElementById("id").value = "";
-                document.getElementById("form-data").reset();
-            }, error : function(response){
-                console.log(response.responseText);
-            }
+                (document.getElementById("jenkel1").checked==true || document.getElementById("jenkel2").checked==true)){
+
+                    $.ajax({
+                        type : 'POST',
+                        url : "form_action.php",
+                        data : data,
+                        success : function(){
+                            $('.data').load("data.php");
+                            document.getElementById("id").value = "";
+                            document.getElementById("form-data").reset();
+                        }, error: function(response){
+                            console.log(response.responseText);
+                        }
+                    });
+                }
+            });
         });
-    }
-});
-});
-    </script>
+    </script>   
 
     <nav class="navbar navbar-dark bg-primary">
         <a class="navbar-brand" href="index.php" style="color : #fff;">
@@ -86,7 +89,7 @@
     </nav>
 
     <div class="container">
-        <h2 align="center" style="margin : 30px;">CRUD Ajax No Loading</h2>
+        <h2 align="center" style="margin : 30px;">CRUD AJAX NO LOADING</h2>
 
         <form method="post" class="form-data" id="form-data">
             <div class="row">
@@ -140,7 +143,7 @@
             </div>
 
             <div class="form-group">
-                <button type="button" name="simpan" class="btn btn-primary">
+                <button type="submit" name="simpan" class="btn btn-primary">
                     <i class="fa fa-save"></i> Simpan
                 </button>
             </div> 
@@ -148,11 +151,19 @@
         <hr>
         <div class="data"></div>
     </div>
-        <div class="text-center">&copy;<?php echo date('Y');?> Copyright:
+        <div class="text-center">&copy;<?php echo date('Y');?> Copyright :
             <a href="https://starbhak.com/">Starbhak Soft</a>
         </div>
 
 </body>
+<style>
+.row{
+    font-family: 'Quicksand', sans-serif;
+}
+div{
+    font-family: 'Quicksand', sans-serif;
+}
+</style>
 
 
 
